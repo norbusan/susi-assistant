2c6d2912921072d035ef2a56602bb043111f3757
Norbert Preining <norbert@preining.info>
2020-09-14 14:34:23 +0900

    update license statement in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Norbert Preining',
     author_email='norbert@preining.info',
 
-    license='GPLv2+',
+    license='Apache License',
 
     python_requires='>=3.6',
 
@@ -46,7 +46,7 @@ setup(
         # 'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL License',
+        'License :: OSI Approved :: Apache License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
